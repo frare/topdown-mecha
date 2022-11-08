56a37a99c8a78a377bc88f22d60478b9ee8a81f2
Lucas Frare <31972242+frare@users.noreply.github.com>
2022-11-08 17:47:48 -0300

    Implementação básica do novo InputSystem

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private InputActionMap moveActions;
+
+    private Vector3 moveInput;
+
+
+
+
+
+    private void Update()
+    {
+        transform.position += moveInput * moveSpeed * Time.deltaTime;
+    }
+
+    #region INPUT SYSTEM ACTION CALLBACKS
+    public void Move(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            moveInput = new Vector3(context.ReadValue<Vector2>().x, 0f, context.ReadValue<Vector2>().y).normalized;
+        }
+        if (context.canceled)
+        {
+            moveInput = Vector3.zero;
+        }
+    }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Attack!");
+        }
+    }
+    #endregion
+}
