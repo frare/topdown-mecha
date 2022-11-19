8360639b53ec64ad0e9eefb1b89dcd8ae2b5b585
Lucas Frare <31972242+frare@users.noreply.github.com>
2022-11-19 20:33:36 -0300

    Inimigo simples adicionado

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Attributes")]
+    [SerializeField] private int health;
+    private int currentHealth;
+    [SerializeField] private float moveSpeed;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Transform model;
+
+
+
+
+
+    private void OnEnable()
+    {
+        currentHealth = health;
+    }
+
+    private void Update()
+    {
+        rb.MovePosition(transform.position + (Player.instance.transform.position - transform.position).normalized * moveSpeed * Time.deltaTime);
+        model.LookAt(Player.instance.transform.position);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0) gameObject.SetActive(false);
+
+        // else give feedback
+    }
+}
