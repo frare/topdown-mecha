0fb02f3ce1a3bcb12f9941713dfbe931c0c865e6
DavidTG98 <davidthiago1998@gmail.com>
2022-11-20 17:52:27 -0300

    (chore): MainMenu

---
+++ Assets/Scripts/Menu/ButtonFeedback.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using FMODUnity;
+
+public class ButtonFeedback : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        RuntimeManager.PlayOneShot("event:/Menu/Button_Hover");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        RuntimeManager.PlayOneShot("event:/Menu/Button_Click");
+    }
+}
