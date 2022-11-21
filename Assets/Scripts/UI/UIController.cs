18ff69819fc4c1c8ed87a6fcc482b520c134b874
Lucas Frare <31972242+frare@users.noreply.github.com>
2022-11-20 22:47:56 -0300

    União do que foi feito até agora

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+
+    [Header("HUD")]
+    [SerializeField] private GameObject hudObject;
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseObject;
+
+
+
+
+
+    #region UNITY CALLBACKS
+    private void Awake()
+    {
+        UIController.instance = this;
+    }
+
+    private void Start()
+    {
+        hudObject.SetActive(true);
+        pauseObject.SetActive(false);
+    }
+    #endregion
+
+    #region CLASS METHODS
+    public static void ShowPauseMenu()
+    {
+        instance.pauseObject.SetActive(true);
+    }
+
+    public static void HidePauseMenu()
+    {
+        instance.pauseObject.SetActive(false);
+    }
+
+    public void OnClick_ResumeButton()
+    {
+        GameController.Resume();
+        instance.pauseObject.SetActive(false);
+    }
+
+    public void OnClick_RestartButton()
+    {
+        GameController.Restart();
+    }
+
+    public void OnClick_MainMenu()
+    {
+        GameController.MainMenu();
+    }
+
+    private IEnumerator FlashHPBar()
+    {
+        yield return null;
+    }
+    #endregion
+}
