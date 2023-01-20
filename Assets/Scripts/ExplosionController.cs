e3c91511583500f18e41cdf604101515aa170d1e
Lucas Frare <31972242+frare@users.noreply.github.com>
2023-01-19 23:54:43 -0300

    Revert "Fix na altura das explosões"
    This reverts commit 1063c072b5ae48ff2619040cc00cb1f85ac6db38.

--- Assets/Scripts/ExplosionController.cs
+++ Assets/Scripts/ExplosionController.cs
@@ -20,7 +20,7 @@ public class ExplosionController : MonoBehaviour
     public static void SpawnExplosion(Vector3 position, Vector3 scale)
     {
         var explosion = instance.pool.GetNext();
-        explosion.transform.position = position + new Vector3(0f, 2.5f, 0f);
+        explosion.transform.position = position;
         explosion.transform.localScale = scale;
         explosion.SetActive(true);
     }
