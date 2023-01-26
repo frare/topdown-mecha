87425ce0f8a59a586fe0b98911dbf40aadda2f28
Lucas Frare <31972242+frare@users.noreply.github.com>
2023-01-25 21:42:32 -0300

    Adicionadas as colisões com o cenário

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -35,10 +35,10 @@ public class Projectile : MonoBehaviour
         distance = Vector3.Distance(nextPosition, transform.position);
 
         // Raycast the bullet forward to check for collisions manually (had better results than Rigidbody)
-        if (Physics.Raycast(new Vector3(transform.position.x, 0f, transform.position.z), direction, out hit, distance, layerMask))
+        if (Physics.Raycast(transform.position, direction, out hit, distance, layerMask))
         {
-            if (hit.collider.gameObject.layer == Enemy.layer) hit.collider.attachedRigidbody.GetComponent<Enemy>()?.TakeDamage(damage);
-            else if (hit.collider.gameObject.layer == EnemyTank.shieldLayer) hit.collider.attachedRigidbody.GetComponent<EnemyTank>()?.ShieldDamaged();
+            if (hit.collider.gameObject.layer == EnemyTank.shieldLayer) hit.collider.attachedRigidbody.GetComponent<EnemyTank>()?.ShieldDamaged();
+            else if (hit.collider.gameObject.layer == Enemy.layer) hit.collider.attachedRigidbody.GetComponent<Enemy>()?.TakeDamage(damage);
 
             gameObject.SetActive(false);
         }
